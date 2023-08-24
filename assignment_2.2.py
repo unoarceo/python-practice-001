b46333f2c023da54e3e86b3ec48c91300eaaf270
Arjay Arceo <arjay.m.arceo@gmail.com>
2023-08-24 16:17:53 +0800

    Updated comments.

--- assignment_2.2.py
+++ assignment_2.2.py
@@ -1,6 +1,4 @@
-#Write a program that uses input to prompt a user for their name and then welcomes them. 
-#Note that input will pop up a dialog box. 
-#Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
+#Prompts for the name and then prints it.
 
 name = input("Enter your name: ")
 print("Hello " + name)
--- assignment_5.2.py
+++ assignment_5.2.py
@@ -1,8 +1,8 @@
 """
-5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
-Once 'done' is entered, print out the largest and smallest of the numbers. 
-If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
-Enter 7, 2, bob, 10, and 4 and match the output below.
+Displays the largest and smallest numbers entered by the user.
+Exits when user enters "done".
+Displays "Invalid input" when user enters non-numerical values.
+Accepts integers only.
 """
 
 def large_test(num_, largest_):
